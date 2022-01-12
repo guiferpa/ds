de493aa2a81dc05496b8e0ea2aab0d86c5231840
Guilherme Paixão <guiferpa@gmail.com>
2022-01-12 16:01:04 -0300

    Added storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-// import styled, { css } from 'styled-components';
+import styled, { css } from 'styled-components';
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> { }
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  label?: string;
+}
 
-// const buttonBaseStyle = (props: ButtonProps) => css``
+const buttonResetStyle = (props: ButtonProps) => css`
+  border: none;
+  margin: 0;
+`
+
+const buttonBaseStyle = (props: ButtonProps) => css`
+  &:hover {
+    cursor: pointer;
+  }
+`
 
 const Button: React.FunctionComponent<ButtonProps> = (props: ButtonProps) => {
   return (
-    <button {...props} >{props.children}</button>
+    <button {...props} >{props.label}</button>
   );
 }
 
-/*
 const ButtonStyled = styled(Button)<ButtonProps>(
+  buttonResetStyle,
   buttonBaseStyle
 );
 
 export default ButtonStyled;
-*/
-
-export default Button;
