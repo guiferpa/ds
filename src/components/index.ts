d13c4ce19575e32429e2c6805b2b17ec39baba4e
Guilherme Paixão <guiferpa@gmail.com>
2022-01-27 23:32:47 -0300

    Exported Icon parts

--- src/components/index.ts
+++ src/components/index.ts
@@ -17,5 +17,6 @@ export { default as UserProfileMenuSection } from './UserProfileMenuSection';
 export { default as Divider } from './Divider';
 export { default as Modal } from './Modal';
 export { default as Alert } from './Alert';
+export * as Icon from './Icon';
 
 export * as Layout from './layouts';
